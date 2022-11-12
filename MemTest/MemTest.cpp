1573ac5acbf72ae7011c73cb5457eea85b734c17
vsudov <vanua.dubinkin@gmail.com>
2022-11-12 11:04:24 +0300

    Initial commit

---
+++ MemTest/MemTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    setlocale(0, "");
+
+    cout << "Введите количество чисел: ";
+    cin >> n;
+
+    int *numbers = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+        numbers[i] = rand();
+        cout << "Записали число в массив: " << numbers[i] << endl;
+    }
+
+    cout << numbers;
+}
